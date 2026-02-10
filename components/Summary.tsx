3c1b2b2571b671008aee72dc46ec1b915077fc8e
Mohammad Asrar <113281948+asrar211@users.noreply.github.com>
2026-02-10 19:41:59 +0530

    first

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -27,7 +27,7 @@ export default function Summary({
         <div className="flex justify-center h-dvh items-center">
             <form className="flex flex-col" onSubmit={handleSumbit}>
                 <textarea
-                className="p-2 border border-neutral-300 text-base outline-0 focus:ring-3 focus:ring-blue-200 w-100 mb-3 h-50 rounded-xl"
+                className="p-2 border border-neutral-300 text-base outline-0 focus:ring-3 focus:ring-blue-200 w-75 mb-3 h-50 rounded-xl"
                 name="summary" placeholder="Enter a small Summary about yourself" value={form.summary} onChange={handleChange}/>
                 <div className="flex gap-3 justify-center items-center">
                     <button className="text-base font-semibold bg-green-400 text-green-900 py-2 rounded-xl w-full" onClick={() => setSteps(prev => prev -1)}>Prev</button>
